d93b799a1c1f440406e70209129cdcd3d4385925
Saymon <saymon.T@outlook.com>
2026-03-19 12:49:34 -0300

    Banco de Dados

---
+++ db/init.sql
@@ -0,0 +1,90 @@
+BEGIN;
+
+CREATE TABLE movies (
+    id SERIAL PRIMARY KEY,
+    tmdb_id INTEGER UNIQUE NOT NULL,
+    titulo_original TEXT,
+
+    titulo_br TEXT,
+    descricao_br TEXT,
+    tagline_br TEXT,
+
+    titulo_en TEXT,
+    descricao_en TEXT,
+    tagline_en TEXT,
+
+    rating NUMERIC(3,1),
+    duracao INTEGER,
+
+    lingua_origem TEXT,
+
+    poster_path_br TEXT,
+    backdrop_path_br TEXT,
+
+    poster_path_us TEXT,
+    backdrop_path_us TEXT,
+
+    homepage TEXT,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX idx_movies_titulo_br ON movies(titulo_br);
+CREATE INDEX idx_movies_titulo_en ON movies(titulo_en);
+
+CREATE TABLE generos (
+    id SERIAL PRIMARY KEY,
+    nome TEXT UNIQUE
+);
+
+CREATE TABLE movie_generos (
+    movie_id INTEGER REFERENCES movies(id) ON DELETE CASCADE,
+    genero_id INTEGER REFERENCES generos(id),
+    PRIMARY KEY (movie_id, genero_id)
+);
+
+CREATE TABLE estudios (
+    id SERIAL PRIMARY KEY,
+    nome TEXT UNIQUE
+);
+
+CREATE TABLE movie_estudios (
+    movie_id INTEGER REFERENCES movies(id) ON DELETE CASCADE,
+    estudio_id INTEGER REFERENCES estudios(id),
+    PRIMARY KEY (movie_id, estudio_id)
+);
+
+CREATE TABLE diretores (
+    id SERIAL PRIMARY KEY,
+    nome TEXT UNIQUE
+);
+
+CREATE TABLE movie_diretores (
+    movie_id INTEGER REFERENCES movies(id) ON DELETE CASCADE,
+    diretor_id INTEGER REFERENCES diretores(id),
+    PRIMARY KEY (movie_id, diretor_id)
+);
+
+CREATE TABLE paises (
+    id SERIAL PRIMARY KEY,
+    nome TEXT UNIQUE
+);
+
+CREATE TABLE movie_paises (
+    movie_id INTEGER REFERENCES movies(id) ON DELETE CASCADE,
+    pais_id INTEGER REFERENCES paises(id),
+    PRIMARY KEY (movie_id, pais_id)
+);
+
+CREATE TABLE colecoes (
+    id SERIAL PRIMARY KEY,
+    tmdb_id INTEGER UNIQUE NOT NULL,
+    nome TEXT,
+    poster_path TEXT,
+    backdrop_path TEXT
+);
+
+-- Adicione uma chave estrangeira na tabela de movies
+ALTER TABLE movies ADD COLUMN colecao_id INTEGER REFERENCES colecoes(id) ON DELETE SET NULL;
+
+COMMIT;
